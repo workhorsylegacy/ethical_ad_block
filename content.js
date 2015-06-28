8dd78855413929f5b717a31eef5c21d81c4a82fb
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2015-06-28 12:58:31 -0700

    Added empty Chrome extension.

---
+++ content.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2015 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
+// This software is licensed under GPL v3 or later
+
+console.log("AHAHHAHAHHAHAHHHHHHHHHHHHHHHHHHH");
+
+
+
---
+++ popup.js
@@ -0,0 +1,4 @@
+// Copyright (c) 2015 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
+// This software is licensed under GPL v3 or later
+
+
