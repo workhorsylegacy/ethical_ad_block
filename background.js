e0d5b45501678486f82c5bf52b81885025bc4745
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2015-07-03 01:57:43 -0700

    Updated so it does not block directly visiting sites in the black list.

--- background.js
+++ background.js
@@ -12,11 +12,13 @@ var BLACKLIST = [
 	'tribalfusion.com',
 	'kontera.com',
 	'admarketplace.net',
-	'exponential.com'
+	'exponential.com',
+	'cpmstar.com'
 ];
 
 var messages = [];
 var is_ready = false;
+var active_url = null;
 
 function log_to_active_tab(message) {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -27,17 +29,22 @@ function log_to_active_tab(message) {
 
 // Watch each request and block the ones that are in the black list
 chrome.webRequest.onBeforeRequest.addListener(function(info) {
-	for (var i=0; i<BLACKLIST.length; ++i) {
-		var entry = BLACKLIST[i];
-		if (info.url.indexOf(entry) !== -1) {
-			var message = 'Blocked: ' + info.url;
-			if (is_ready) {
-				log_to_active_tab(message);
-			} else {
-				messages.push(message);
-			}
+	if (active_url !== null) {
+		for (var i=0; i<BLACKLIST.length; ++i) {
+			var entry = BLACKLIST[i];
+			// Block the request if it is to a black listed site,
+			// But not if the current page is the black listed site
+			if (info.url.indexOf(entry) !== -1 && active_url.indexOf(entry) === -1) {
+				var message = 'Blocked: ' + info.url + ', ' + active_url;
 
-			return {cancel: true};
+				if (is_ready) {
+					log_to_active_tab(message);
+				} else {
+					messages.push(message);
+				}
+
+				return {cancel: true};
+			}
 		}
 	}
 
@@ -47,6 +54,8 @@ chrome.webRequest.onBeforeRequest.addListener(function(info) {
 
 // When the tab is ready, print all the log messages to its console
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+	active_url = tab.url;
+
 	if (changeInfo.status == 'complete') {
 		is_ready = true;
 		for (var i=0; i<messages.length; ++i) {
