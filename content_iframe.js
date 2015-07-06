065a364a8c95a7aecc302b686a281dfb20bb0a1b
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2015-07-06 15:20:22 -0700

    Updated the iframe hashing to include images.

--- content_iframe.js
+++ content_iframe.js
@@ -8,11 +8,36 @@ if (window.location !== window.parent.location) {
 	window.addEventListener('message', function(event) {
 		// Hashing the iframe
 		if (event.data === 'close_iframe') {
+			// Create a hash of the iframe
 			var serializer = new XMLSerializer();
-			var content = serializer.serializeToString(document);
-			var hash = hex_md5(content);
-			// Post the hash back to the parent page
-			window.parent.postMessage(hash, '*');
+			var hash = serializer.serializeToString(document);
+			var imgs = document.getElementsByTagName('img');
+			var count_down = imgs.length;
+			for (var i=0; i<imgs.length; ++i) {
+				var iframe_img = imgs[i];
+				var img = new Image;
+				img.crossOrigin = 'Anonymous';
+				img.onload = function() {
+					// Create a hash of the image
+					var temp_canvas = document.createElement('canvas');
+					temp_canvas.width = img.width;
+					temp_canvas.height = img.height;
+					var ctx = temp_canvas.getContext('2d');
+					ctx.drawImage(img, 0, 0);
+					var data_url = temp_canvas.toDataURL();
+					hash += iframe_img.outerHTML + data_url;
+					count_down -= 1;
+
+					// If this is the last image to load,
+					// post the hash back to the parent page
+					if (count_down == 0) {
+						//console.log(hash);
+						hash = hex_md5(hash);
+						window.parent.postMessage(hash, '*');
+					}
+				};
+				img.src = iframe_img.src;
+			}
 		}
 	});
 }
