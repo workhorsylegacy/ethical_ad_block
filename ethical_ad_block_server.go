42d7a4a4c1e24a6874c14325adf988a35be12119
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2015-08-08 18:16:13 -0700

    Updated the server to have individual user vote counts.

--- ethical_ad_block_server.go
+++ ethical_ad_block_server.go
@@ -13,10 +13,24 @@ import (
 	"time"
 )
 
-var ads_good map[string]uint64
-var ads_fraudulent map[string]uint64
-var ads_taxing map[string]uint64
-var ads_malicious map[string]uint64
+type AdData struct {
+	good map[string]uint64
+	fraudulent map[string]uint64
+	taxing map[string]uint64
+	malicious map[string]uint64
+}
+
+func NewAdData() *AdData {
+	self := new(AdData)
+	self.good = make(map[string]uint64)
+	self.fraudulent = make(map[string]uint64)
+	self.taxing = make(map[string]uint64)
+	self.malicious = make(map[string]uint64)
+	return self
+}
+
+var user_ads map[string]*AdData
+var all_ads *AdData
 var user_ids map[string]time.Time
 
 func httpCB(w http.ResponseWriter, r *http.Request) {
@@ -30,13 +44,27 @@ func httpCB(w http.ResponseWriter, r *http.Request) {
 		ad_type := values["ad_type"][0]
 		user_id := values["user_id"][0]
 
+		// Initialize space for this user's ads
+		if _, ok := user_ads[user_id]; ! ok {
+			user_ads[user_id] = NewAdData()
+		}
+
 		// Figure out which type of vote it will be
 		var ad_map *map[string]uint64
+		var ad_map_user *map[string]uint64
 		switch ad_type {
-			case "good": ad_map = &ads_good
-			case "fraudulent": ad_map = &ads_fraudulent
-			case "taxing": ad_map = &ads_taxing
-			case "malicious": ad_map = &ads_malicious
+			case "good":
+				ad_map = &all_ads.good
+				ad_map_user = &(user_ads[user_id].good)
+			case "fraudulent":
+				ad_map = &all_ads.fraudulent
+				ad_map_user = &(user_ads[user_id].fraudulent)
+			case "taxing":
+				ad_map = &all_ads.taxing
+				ad_map_user = &(user_ads[user_id].taxing)
+			case "malicious":
+				ad_map = &all_ads.malicious
+				ad_map_user = &(user_ads[user_id].malicious)
 			default:
 				fmt.Fprintf(w, "Invalid ad_type\n")
 				return
@@ -44,34 +72,36 @@ func httpCB(w http.ResponseWriter, r *http.Request) {
 
 		// Cast the vote
 		(*ad_map)[ad_id] += 1
+		(*ad_map_user)[ad_id] += 1
 		votes := (*ad_map)[ad_id]
+		user_votes := (*ad_map_user)[ad_id]
 
 		// Save the time that the user voted
 		user_ids[user_id] = time.Now()
 
 		// Return the response
-		fmt.Fprintf(w, "ad_id:%s, ad_type:%s, votes:%d\n", ad_id, ad_type, votes)
+		fmt.Fprintf(w, "ad_id:%s, ad_type:%s, votes:%d, user_votes:%d\n", ad_id, ad_type, votes, user_votes)
 
 	// List ads request
 	} else if _, ok := values["list"]; ok {
 		// Print the values of all the ad maps
-		fmt.Fprintf(w, "ads_good:\n")
-		for ad_id, votes := range ads_good {
+		fmt.Fprintf(w, "good:\n")
+		for ad_id, votes := range all_ads.good {
 			fmt.Fprintf(w, "    %s : %d\n", ad_id, votes)
 		}
 
-		fmt.Fprintf(w, "ads_fraudulent:\n")
-		for ad_id, votes := range ads_fraudulent {
+		fmt.Fprintf(w, "fraudulent:\n")
+		for ad_id, votes := range all_ads.fraudulent {
 			fmt.Fprintf(w, "    %s : %d\n", ad_id, votes)
 		}
 
-		fmt.Fprintf(w, "ads_taxing:\n")
-		for ad_id, votes := range ads_taxing {
+		fmt.Fprintf(w, "taxing:\n")
+		for ad_id, votes := range all_ads.taxing {
 			fmt.Fprintf(w, "    %s : %d\n", ad_id, votes)
 		}
 
-		fmt.Fprintf(w, "ads_malicious:\n")
-		for ad_id, votes := range ads_malicious {
+		fmt.Fprintf(w, "malicious:\n")
+		for ad_id, votes := range all_ads.malicious {
 			fmt.Fprintf(w, "    %s : %d\n", ad_id, votes)
 		}
 
@@ -83,10 +113,8 @@ func httpCB(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	// Initialize all the maps
-	ads_good = make(map[string]uint64)
-	ads_fraudulent = make(map[string]uint64)
-	ads_taxing = make(map[string]uint64)
-	ads_malicious = make(map[string]uint64)
+	user_ads = make(map[string]*AdData)
+	all_ads = NewAdData()
 	user_ids = make(map[string]time.Time)
 
 	// Get the port number
