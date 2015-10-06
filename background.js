3dfe1bc31b36df221d6dce6d9ef0d08a1db3d222
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2015-10-06 14:17:56 -0700

    Changed more if statements to switches.

--- background.js
+++ background.js
@@ -47,55 +47,61 @@ chrome.webRequest.onBeforeRequest.addListener(function(info) {
 
 // FIXME: Update this to use send_response instead of sending another message, instead of return false
 chrome.runtime.onMessage.addListener(function(msg, sender, send_response) {
-	if (msg.action === 'get_voted_ad_type') {
-		var ad_id = msg.ad_id;
-		var voted_ad_type = null;
-		if (g_response_cache.hasOwnProperty(ad_id)) {
-			voted_ad_type = g_response_cache[ad_id];
-		}
-
-		var message = {
-			action: 'get_voted_ad_type',
-			voted_ad_type: voted_ad_type
-		};
-		send_response(message);
-	// FIXME: Update this to limit the size of the cache
-	// FIXME: Update this to expire items in the cache after 5 minutes or so
-	} else if (msg.action === 'set_voted_ad_type') {
-		var ad_id = msg.ad_id;
-		var voted_ad_type = msg.voted_ad_type;
-		g_response_cache[ad_id] = voted_ad_type;
-		return false;
-	} else if (msg.action === 'remove_voted_ad_type') {
-		var ad_id = msg.ad_id;
-		delete g_response_cache[ad_id];
-		var message = {
-			action: 'remove_voted_ad_type'
-		};
-		send_response(message);
-	} else if (msg.action === 'screen_shot') {
-		var rect = msg.rect;
-
-		// Screen capture the tab and send it to the tab's console
-		chrome.tabs.captureVisibleTab(
-			null,
-			{'format': 'png'},
-			function(data_url) {
-				var message = {
-					action: 'screen_shot',
-					data: data_url
-				};
-				chrome.tabs.sendMessage(sender.tab.id, message, function(response) {});
+	switch (msg.action ) {
+		case 'get_voted_ad_type':
+			var ad_id = msg.ad_id;
+			var voted_ad_type = null;
+			if (g_response_cache.hasOwnProperty(ad_id)) {
+				voted_ad_type = g_response_cache[ad_id];
 			}
-		);
-		return false;
-	} else if (msg.action === 'get_g_user_id') {
-		var message = {
-			action: 'get_g_user_id',
-			data: g_user_id
-		};
-		chrome.tabs.sendMessage(sender.tab.id, message, function(response) {});
-		return false;
+
+			var message = {
+				action: 'get_voted_ad_type',
+				voted_ad_type: voted_ad_type
+			};
+			send_response(message);
+			break;
+		// FIXME: Update this to limit the size of the cache
+		// FIXME: Update this to expire items in the cache after 5 minutes or so
+		case 'set_voted_ad_type':
+			var ad_id = msg.ad_id;
+			var voted_ad_type = msg.voted_ad_type;
+			g_response_cache[ad_id] = voted_ad_type;
+			return false;
+			break;
+		case 'remove_voted_ad_type':
+			var ad_id = msg.ad_id;
+			delete g_response_cache[ad_id];
+			var message = {
+				action: 'remove_voted_ad_type'
+			};
+			send_response(message);
+			break;
+		case 'screen_shot':
+			var rect = msg.rect;
+
+			// Screen capture the tab and send it to the tab's console
+			chrome.tabs.captureVisibleTab(
+				null,
+				{'format': 'png'},
+				function(data_url) {
+					var message = {
+						action: 'screen_shot',
+						data: data_url
+					};
+					chrome.tabs.sendMessage(sender.tab.id, message, function(response) {});
+				}
+			);
+			return false;
+			break;
+		case 'get_g_user_id':
+			var message = {
+				action: 'get_g_user_id',
+				data: g_user_id
+			};
+			chrome.tabs.sendMessage(sender.tab.id, message, function(response) {});
+			return false;
+			break;
 	}
 });
 
